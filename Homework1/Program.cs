3a47fb6c55ab0d9adb093208925a192ae280040f
Miha <Miha@DESKTOP-5T344OH>
2018-12-04 12:56:42 -0800

    Guess the number game

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -6,7 +6,40 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random randomNumber = new Random();
+
+            int numberToGuess = randomNumber.Next(1, 100);
+            int userGuess = 0;
+            bool match = false;
+
+            Console.WriteLine("I have in mind a number between 1 and 100. Can you guess it ? :)");
+
+            while (!match)
+            {
+                Console.Write("Your guess is: ");
+                string userInput = Console.ReadLine();
+
+                if (!int.TryParse(userInput, out userGuess))
+                {
+                    Console.WriteLine("That's not a number. Please enter a number.");
+                    continue;
+                }
+
+                if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("The number I have in mind is higher than the number you entered. Please try again.");
+                }
+                else if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("The number I have in mind is lower than the number you entered. Please try again.");
+                }
+                else
+                {
+                    match = true;
+                    Console.WriteLine("Yay, you guessed!");
+                }
+            }
         }
+
     }
 }
